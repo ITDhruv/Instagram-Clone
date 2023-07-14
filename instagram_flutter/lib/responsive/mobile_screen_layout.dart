ee30d2cc0bd4d6dc7d10b10af7cd2e5f6f7a485d
ITDhruv <120175094+ITDhruv@users.noreply.github.com>
2023-07-15 01:52:33 +0530

    💄 Post Screen UI

--- instagram_flutter/lib/responsive/mobile_screen_layout.dart
+++ instagram_flutter/lib/responsive/mobile_screen_layout.dart
@@ -4,6 +4,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/utils/colors.dart';
+import 'package:instagram_flutter/utils/global_variables.dart';
 
 class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({super.key});
@@ -41,13 +42,7 @@ class _MobileScreenLayoutState extends State<MobileScreenLayout> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
-        children: [
-          Text('feed'),
-          Text('search'),
-          Text('add post'),
-          Text('notif'),
-          Text('profile'),
-        ],
+        children: homeScreenItems,
         physics: const NeverScrollableScrollPhysics(),
         controller: pageController,
         onPageChanged: onPageChanged,
